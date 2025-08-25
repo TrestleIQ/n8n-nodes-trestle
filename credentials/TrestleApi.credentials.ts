f6bc182aa5899a1f3b3724c926b2d7fcb81683ba
shivang-trestle <schheda@trestleiq.com>
2025-08-26 02:15:20 +0530

    added credential test for library issues on the repo and updated version name

--- credentials/TrestleApi.credentials.ts
+++ credentials/TrestleApi.credentials.ts
@@ -1,5 +1,6 @@
 import {
 	IAuthenticateGeneric,
+	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
 } from 'n8n-workflow';
@@ -7,8 +8,13 @@ import {
 export class TrestleApi implements ICredentialType {
 	name = 'trestleApi';
 	displayName = 'Trestle API';
+
 	documentationUrl = 'https://trestle-api.redoc.ly/Current/tag/Phone-Validation-API';
+
 	properties: INodeProperties[] = [
+		// The credentials to get from user and save encrypted.
+		// Properties can be defined exactly in the same way
+		// as node properties.
 		{
 			displayName: 'API Key',
 			name: 'apiKey',
@@ -22,12 +28,23 @@ export class TrestleApi implements ICredentialType {
 		},
 	];
 
+	// This credential is currently not used by any node directly
+	// but the HTTP Request node can use it to make requests.
+	// The credential is also testable due to the `test` property below
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			headers: {
 				'x-api-key': '={{ $credentials.apiKey }}',
 			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.trestleiq.com/',
+			url: '/3.0/phone_intel?phone=2069735100',
 		},
 	};
 }
